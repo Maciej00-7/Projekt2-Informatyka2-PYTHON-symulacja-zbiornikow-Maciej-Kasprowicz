62745c976fe418de5155006d52bb8f0e2e0aafc0
Maciej Kasprowicz <115074758+Maciej00-7@users.noreply.github.com>
2026-01-12 21:19:28 +0100

    wersja 0.9
    Został dodany czwarty zbiornik, przyciski umożliwiające napełnienie oraz opróżnienie stanu wody każdego ze zbiorników, zawór za drugim zbiornikiem odpowiadający za kierunek przepływu wody, lista odpowiadająca za  nastawienie stanu zaworu

--- projekt2_scad.py
+++ projekt2_scad.py
@@ -1,16 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QComboBox, QWidget, QPushButton, QLabel
 from PyQt5.QtCore import Qt, QTimer, QPointF
 from PyQt5.QtGui import QPainter, QColor, QPen, QPainterPath
 class Rura:
 
     def __init__(self, punkty, grubosc=12, kolor=Qt.gray):
 
-        # Konwersja listy krotek na obiekty QPointF
         self.punkty = [QPointF(float(p[0]), float(p[1])) for p in punkty]
         self.grubosc = grubosc
         self.kolor_rury = kolor
-        self.kolor_cieczy = QColor(0, 180, 255) # Jasny niebieski
+        self.kolor_cieczy = QColor(0, 180, 255) 
         self.czy_plynie = False
 
     def ustaw_przeplyw(self, plynie):
@@ -67,14 +66,16 @@ class Zbiornik:
 
     def punkt_gora_srodek(self): return (self.x + self.width/2, self.y)
     def punkt_dol_srodek(self): return (self.x + self.width/2, self.y + self.height)
+
     def draw(self, painter):
         # 1. Rysowanie cieczy
         if self.poziom > 0:
             h_cieczy = self.height * self.poziom
             y_start = self.y + self.height- h_cieczy
+            wysokosc_rysowania = max(1, int(h_cieczy - 2))
             painter.setPen(Qt.NoPen)
             painter.setBrush(QColor(0, 120, 255, 200))
-            painter.drawRect(int(self.x + 3), int(y_start), int(self.width- 6),int(h_cieczy- 2))
+            painter.drawRect(int(self.x + 3), int(y_start), int(self.width- 6), wysokosc_rysowania)
             # 2. Rysowanie obrysu
         pen = QPen(Qt.white, 4)
         pen.setJoinStyle(Qt.MiterJoin)
@@ -83,50 +84,110 @@ class Zbiornik:
         painter.drawRect(int(self.x), int(self.y), int(self.width), int(self.height))
         # 3. Podpis
         painter.setPen(Qt.white)
-        painter.drawText(int(self.x), int(self.y- 10), self.nazwa)
+        painter.drawText(int(self.x), int(self.y- 20), self.nazwa)
 class SymulacjaKaskady(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Kaskada: Dol-> Gora")
-        self.setFixedSize(900, 600)
+        self.setFixedSize(1100, 700)
         self.setStyleSheet("background-color: #222;")
 
-        #---Konfiguracja Zbiornikow (schodkowo)--
-        self.z1 = Zbiornik(50, 50, nazwa="Zbiornik 1")
+        # Tryb zaworu (0 = Oba, 1 = Tylko Z3, 2 = Tylko Z4)
+        self.tryb_zaworu = 0
+
+        #Konfiguracja Zbiornikow
+        self.z1 = Zbiornik(200, 50, nazwa="Zbiornik 1")
         self.z1.aktualna_ilosc = 100.0; self.z1.aktualizuj_poziom() # Pelny
         
-        self.z2 = Zbiornik(350, 200, nazwa="Zbiornik 2")
-        self.z3 = Zbiornik(650, 350, nazwa="Zbiornik 3")
-        self.zbiorniki = [self.z1, self.z2, self.z3]
-        #---Konfiguracja Rur--
+        self.z2 = Zbiornik(530, 200, nazwa="Zbiornik 2")
+        self.z3 = Zbiornik(860, 350, nazwa="Zbiornik 3")
+        self.z4 = Zbiornik(200, 350, nazwa="Zbiornik 4")
+
+        self.zbiorniki = [self.z1, self.z2, self.z3, self.z4]
+
+        #Konfiguracja Rur
         # Rura 1: Z1 (Dol)-> Z2 (Gora)
-        p_start = self.z1.punkt_dol_srodek()
-        p_koniec = self.z2.punkt_gora_srodek()
-        mid_y = (p_start[1] + p_koniec[1]) / 2
+        p1_start = self.z1.punkt_dol_srodek()
+        p1_koniec = self.z2.punkt_gora_srodek()
+        mid_y = (p1_start[1] + p1_koniec[1]) / 2
 
-        self.rura1 = Rura([
-            p_start, (p_start[0], mid_y), (p_koniec[0], mid_y), p_koniec])
+        self.rura1 = Rura([p1_start, (p1_start[0], mid_y), (p1_koniec[0], mid_y), p1_koniec])
 
-        # Rura 2: Z2 (Dol)-> Z3 (Gora)
-        p_start2 = self.z2.punkt_dol_srodek()
-        p_koniec2 = self.z3.punkt_gora_srodek()
-        mid_y2 = (p_start2[1] + p_koniec2[1]) / 2
+        # Rura 2: Z2 (Dol)-> Z3 (prawo)
+        p2_start = self.z2.punkt_dol_srodek()
+        p2_koniec = self.z3.punkt_gora_srodek()
+        mid_y2 = (p2_start[1] + p2_koniec[1]) / 2
 
-        self.rura2 = Rura([
-            p_start2, (p_start2[0], mid_y2), (p_koniec2[0], mid_y2), p_koniec2])
-        self.rury = [self.rura1, self.rura2]
+        self.rura2 = Rura([p2_start, (p2_start[0], mid_y2), (p2_koniec[0], mid_y2), p2_koniec])
+        # Rura 3: Z2 -> Z4 (W lewo)
+        p3_start = self.z2.punkt_dol_srodek() 
+        p3_koniec = self.z4.punkt_gora_srodek()
+        mid_y3 = (p3_start[1] + p3_koniec[1]) / 2
 
-        #---Timer i Sterowanie--
+        self.rura3 = Rura([p3_start,(p3_start[0], mid_y3 ), (p3_koniec[0], mid_y3),p3_koniec])
+
+        self.rury = [self.rura1, self.rura2, self.rura3]
+
+        #Timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.logika_przeplywu)
-        
-        self.btn = QPushButton("Start / Stop", self)
-        self.btn.setGeometry(50, 550, 100, 30)
-        self.btn.setStyleSheet("background-color: #444; color: white;")
-        self.btn.clicked.connect(self.przelacz_symulacje)
-        
         self.running = False
         self.flow_speed = 0.8
+        self.setup_interfejs()
+
+    def setup_interfejs(self):
+        # 1. Główny przycisk Start/Stop
+        self.btn_start = QPushButton("Start / Stop", self)
+        self.btn_start.setGeometry(50, 550, 100, 40)
+        self.btn_start.setStyleSheet("background-color: #444; color: white; font-weight: bold;")
+        self.btn_start.clicked.connect(self.przelacz_symulacje)
+
+        # 2. Generowanie paneli sterowania dla każdego zbiornika
+        for i, zbiornik in enumerate(self.zbiorniki):
+            base_x = 200 + (i * 220) 
+            base_y = 540
+
+            # Etykieta
+            lbl = QLabel(zbiornik.nazwa, self)
+            lbl.move(base_x, base_y - 20)
+            lbl.setStyleSheet("color: #AAA;")
+
+            # Przycisk [+] Napełnij
+            btn_fill = QPushButton("[+] Napełnij", self)
+            btn_fill.setGeometry(base_x, base_y, 100, 25)
+            btn_fill.setStyleSheet("background-color: green; color: white;") 
+            # Przekazujemy konkretny obiekt zbiornika i True (napełnij)
+            btn_fill.clicked.connect(lambda checked, z=zbiornik: self.steruj_zbiornikiem(z, pelny=True))
+
+            # Przycisk [-] Opróżnij
+            btn_empty = QPushButton("[-] Opróżnij", self)
+            btn_empty.setGeometry(base_x, base_y + 30, 100, 25)
+            btn_empty.setStyleSheet("background-color: red; color: white;") 
+            # Przekazujemy konkretny obiekt zbiornika i False (opróżnij)
+            btn_empty.clicked.connect(lambda checked, z=zbiornik: self.steruj_zbiornikiem(z, pelny=False))
+
+            # STEROWANIE ZAWOREM dla Zbiornika 2
+            if i == 1:
+                lbl_v = QLabel("Ustawienie zaworu:", self)
+                lbl_v.move(base_x, base_y + 65)
+                lbl_v.setStyleSheet("font-size: 10px; color: #BBB;")
+
+                self.combo = QComboBox(self)
+                self.combo.addItems(["Rozpływ w obie strony", "Rozpływ w Prawo", "Rozpływ w Lewo", "Zamknięty"])
+                self.combo.setGeometry(base_x, base_y + 80, 140, 25)
+                self.combo.setStyleSheet("background-color: #555; selection-background-color: #0088cc;")
+                self.combo.currentIndexChanged.connect(self.zmiana_zaworu)
+    
+    def steruj_zbiornikiem(self, zbiornik, pelny):
+        if pelny:
+            zbiornik.aktualna_ilosc = zbiornik.pojemnosc
+        else:
+            zbiornik.aktualna_ilosc = 0.0
+        
+        zbiornik.aktualizuj_poziom()
+        self.update()
+    def zmiana_zaworu(self, index):
+        self.tryb_zaworu = index
 
     def przelacz_symulacje(self):
         if self.running: self.timer.stop()
@@ -134,21 +195,62 @@ class SymulacjaKaskady(QWidget):
         self.running = not self.running
 
     def logika_przeplywu(self):
-        # 1. Przeplyw Z1-> Z2
+        # 1. Przepływ Z1 -> Z2
         plynie_1 = False
         if not self.z1.czy_pusty() and not self.z2.czy_pelny():
             ilosc = self.z1.usun_ciecz(self.flow_speed)
             self.z2.dodaj_ciecz(ilosc)
             plynie_1 = True
-            self.rura1.ustaw_przeplyw(plynie_1)
-            # 2. Przeplyw Z2-> Z3 (Startuje dopiero gdy Z2 ma troche wody)
-            plynie_2 = False
-            if self.z2.aktualna_ilosc > 5.0 and not self.z3.czy_pelny():
-                ilosc = self.z2.usun_ciecz(self.flow_speed)
-                self.z3.dodaj_ciecz(ilosc)
-                plynie_2 = True
-            self.rura2.ustaw_przeplyw(plynie_2)
-            self.update()
+        self.rura1.ustaw_przeplyw(plynie_1)
+
+        #Rura do Z3
+        plynie_do_z3 = False
+        plynie_do_z4 = False
+        if self.z2.aktualna_ilosc > 0.1:
+            otwarty_z3 = not self.z3.czy_pelny()
+            otwarty_z4 = not self.z4.czy_pelny()
+
+            # TRYB 0: RÓWNOMIERNIE 
+            if self.tryb_zaworu == 0:
+                if otwarty_z3 and otwarty_z4:
+                    do_pobrania = min(self.z2.aktualna_ilosc, self.flow_speed * 2)
+                    pobrana = self.z2.usun_ciecz(do_pobrania)
+                    self.z3.dodaj_ciecz(pobrana / 2)
+                    self.z4.dodaj_ciecz(pobrana / 2)
+                    plynie_do_z3 = True; plynie_do_z4 = True
+                elif otwarty_z3: 
+                    pobrana = self.z2.usun_ciecz(min(self.z2.aktualna_ilosc, self.flow_speed))
+                    self.z3.dodaj_ciecz(pobrana)
+                    plynie_do_z3 = True
+                elif otwarty_z4: 
+                    pobrana = self.z2.usun_ciecz(min(self.z2.aktualna_ilosc, self.flow_speed))
+                    self.z4.dodaj_ciecz(pobrana)
+                    plynie_do_z4 = True
+
+            #TRYB 1: TYLKO Z3
+            elif self.tryb_zaworu == 1:
+                if otwarty_z3:
+                    pobrana = self.z2.usun_ciecz(min(self.z2.aktualna_ilosc, self.flow_speed))
+                    self.z3.dodaj_ciecz(pobrana)
+                    plynie_do_z3 = True
+               
+
+            #TRYB 2: TYLKO Z4
+            elif self.tryb_zaworu == 2:
+                if otwarty_z4:
+                    pobrana = self.z2.usun_ciecz(min(self.z2.aktualna_ilosc, self.flow_speed))
+                    self.z4.dodaj_ciecz(pobrana)
+                    plynie_do_z4 = True
+                    
+            #TRYB 3: ZAMKNIĘTY
+            elif self.tryb_zaworu == 3:
+                pass
+                
+
+        self.rura2.ustaw_przeplyw(plynie_do_z3)
+        self.rura3.ustaw_przeplyw(plynie_do_z4)
+            
+        self.update()
     def paintEvent(self, event):
         p = QPainter(self)
         p.setRenderHint(QPainter.Antialiasing)
